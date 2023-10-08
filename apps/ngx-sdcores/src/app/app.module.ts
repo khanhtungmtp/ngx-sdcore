ab6b3572d081a7bbea8deeec357057536b39ade4
khanhtung97 <umlatao97@gmail.com>
2023-10-08 22:42:15 +0700

    sdcores lib build with nx workspace

--- apps/ngx-sdcores/src/app/app.module.ts
+++ apps/ngx-sdcores/src/app/app.module.ts
@@ -4,14 +4,42 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { NgxTrimInputDirective } from 'ngx-sdcores/sd-shared';
+import { SnotifyModule, ToastDefaults, SnotifyService } from 'ngx-sdcores/sd-snotify';
+import { NgxSpinnerModule } from 'ngx-spinner';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NgxTrimInputDirective],
   imports: [
+    BrowserAnimationsModule,
+
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserModule,
+    SnotifyModule,
+    NgxSpinnerModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: 'SnotifyToastConfig', useValue: ToastDefaults },
+    SnotifyService,
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
